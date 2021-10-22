0b748f8ae9897edb55fc24bd7bfdb63350e20577
Anigabi <anag1559@gmail.com>
2021-10-22 12:53:46 +0000

    icono Ana

--- src/app.js
+++ src/app.js
@@ -7,3 +7,11 @@ import "./assets/img/4geeks.ico";
 
 //JUST CODE BELOW HERE
 window.onload = function() {};
+
+const BUTTON = document.querySelector("#buttonClick");
+const ICON = document.querySelector(".Anigabi");
+
+BUTTON.addEventListener("click", () => {
+  ICON.innerHTML = " 👩🏽‍💻";
+  ICON.classList.toggle("auraGlow");
+});
