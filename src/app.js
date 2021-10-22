9dacd2e0e263826f3a631192765a32b718c50551
willy5360 <willy5360@hotmail.com>
2021-10-22 10:56:41 +0000

    finished problem2

--- src/app.js
+++ src/app.js
@@ -5,5 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const EMOTICON = document.querySelector(".RubenWilly");
+const BUTTON = document.querySelector("#buttonClick");
+
 //JUST CODE BELOW HERE
 window.onload = function() {};
+BUTTON.addEventListener("click", () => {
+  EMOTICON.innerHTML = "🕹";
+  EMOTICON.classList.toggle("auraGlow");
+});
