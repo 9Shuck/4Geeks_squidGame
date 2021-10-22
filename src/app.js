1fd6c25151eba496ee4434fa998e9009664f65db
Carolth7 <carol.cth93@gmail.com>
2021-10-22 13:31:44 +0000

    por fin

--- src/app.js
+++ src/app.js
@@ -7,3 +7,16 @@ import "./assets/img/4geeks.ico";
 
 //JUST CODE BELOW HERE
 window.onload = function() {};
+
+window.onload = function() {};
+
+const CLASS = document.querySelector(".Carolth7");
+
+const BOTTON = document.querySelector("#buttonClick");
+
+BOTTON.addEventListener("click", event => {
+  event.preventDefault();
+  let emotic = "🙌🏼";
+  CLASS.innerHTML = emotic;
+  CLASS.classList.toggle("auraGlow");
+});
